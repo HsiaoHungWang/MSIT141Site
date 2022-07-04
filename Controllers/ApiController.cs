4ff4285105d2652e51be4698e28262d264727f0f
hhwang <wang@ispan.com.tw>
2022-07-04 11:46:20 +0800

    資料的傳送與接收

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -8,9 +8,14 @@ namespace MSIT141Site.Controllers
 {
     public class ApiController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string name,int age=0)
         {
-            return Content("Ajax, 你好!!","text/plain", System.Text.Encoding.UTF8);
+            //return Content("Ajax, 你好!!","text/plain", System.Text.Encoding.UTF8);
+            if (String.IsNullOrEmpty(name))
+            {
+                name = "Ajax";
+            }
+            return Content($"{name}你好,你的年紀是{age}!!", "text/plain", System.Text.Encoding.UTF8);
         }
     }
 }
